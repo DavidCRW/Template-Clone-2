7550df1c69c2308e80aee2d27c160e12bb6fd867
DavidCRW <vargadavid946@gmail.com>
2022-09-09 12:54:01 +0200

    upload

---
+++ script/app.js
@@ -0,0 +1,4 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+hamburgerMenu.addEventListener('click', () => {mobileMenu.classList.toggle("show")})
